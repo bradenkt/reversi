1130b1a797df5cfd103e8b1de9adfe8ea8d7d211
Braden Thuraisingham <me@bradenkt.com>
2020-05-18 00:13:02 -0400

    lobby.html updates

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+// this is the main.js file
+
+function getURLParameters(whichParam) 
+{
+	var pageURL = window.location.search.substring(1);
+	var pageURLVariables = pageURL.split('&');
+	for(var i = 0; i < pageURLVariables.length; i++){
+		var parameterName = pageURLVariables[i].split('=');
+		if(parameterName[0] === whichParam){
+			return parameterName[1];
+		}
+	}
+}
+var username = getURLParameters('username');
+if('undefined' === typeof username || !username){
+	username = 'Anonymous_'+Math.random();
+}
+
+$('#messages').append('<h4>'+getURLParameters('username')+'</h4>');
